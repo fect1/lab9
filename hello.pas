d118861591c9be936d4b9694adfd48f8452ead7c
fect1 <RusiSLin@gmail.com>
2024-10-10 18:20:34 +0300

    добавил hello.pas

---
+++ hello.pas
@@ -0,0 +1,8 @@
+﻿﻿begin
+  var lang := readinteger('Введите ваш язык');
+   case lang of 
+    'english','eng','en':Print('Hello World');
+    'russian','rus','ru','русский':Print('Привет мир');
+    'french','fr','Français':Print('Bonjour le monde');
+  end;
+end.
